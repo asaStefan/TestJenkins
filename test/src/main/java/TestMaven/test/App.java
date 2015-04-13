81ad62f60f505c69413c4fca11948827c2e40748
asaStefan <ztq0701@163.com>
2015-04-13 14:33:54 -0400

    app error

--- test/src/main/java/TestMaven/test/App.java
+++ test/src/main/java/TestMaven/test/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
        new App().sayHello();
+       new App().sayError();
     }
     public void sayHello(){
     	 System.out.println( "Hello World!" );
